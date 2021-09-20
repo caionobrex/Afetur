2caf333fbaa36b79944b42e8c3a0e83bf5e699a6
Caio Nobre <marialoucos@hotmail.com.br>
2021-09-20 10:49:41 -0300

    Timestamp added to testimonial model

--- models/Testimonial.js
+++ models/Testimonial.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose'
 const testimonialSchema = new mongoose.Schema({
   name: { type: String, required: true },
   statment: { type: String, required: true }
-})
+}, { timestamps: true })
 
-export default mongoose.model('Testimonial', testimonialSchema)
+export default mongoose.models.Testimonial || mongoose.model('Testimonial', testimonialSchema)
